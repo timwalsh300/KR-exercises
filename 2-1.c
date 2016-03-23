0e0e6e8af52e00c3d2f2fa94e46baf19898005bb
timwalsh300 <timwalsh300@gmail.com>
2016-03-22 21:27:59 -0500

    Computed range of all types except float and double

--- 2-1.c
+++ 2-1.c
@@ -8,7 +8,7 @@
 
 int main() {
 
-	printf("\n\nRange numerical data types according to the standard library...\n\n");
+	printf("\n\nRange of numerical data types according to the standard library...\n\n");
 	printf("Max char, signed = %d\n", SCHAR_MAX);
 	printf("Min char, signed = %d\n", SCHAR_MIN);
 	printf("Max char, unsigned = %u\n\n", UCHAR_MAX);
@@ -26,20 +26,47 @@ int main() {
 	printf("Max double, signed = %e\n", DBL_MAX);
 	printf("Min double, signed = %e\n\n", DBL_MIN);
 
-	printf("\nRange numerical data types, computed...\n\n");
+	printf("\nRange of numerical data types, computed...\n\n");
 	signed char c;
-	/* for some reason, this doesn't work because (c + 1 > 0) evaluates to 'true'
+	/* for some reason, this code doesn't work because (c + 1 > 0) evaluates to 'true'
 	** even when c = SCHAR_MAX...
 	** for (c = 1; c + 1 > 0; c++);
 	** printf("Max char, signed = %d\n", c);
-	** so we need to keep adding until c < 0 and then decrement before printing
+	** so we need to keep adding until c < 0 and then decrement before printing...
 	*/
 	for (c = 1; c > 0; c++);
 	printf("Max char, signed = %d\n", --c);
 	for (c = -1; c < 0; c--);
 	printf("Min char, signed = %d\n", ++c);
-	/*unsigned char uc;
-	for (uc = 0; uc + 1 > 0; uc++);
-	printf("Max char, unsigned = %u\n\n", uc);*/
+	unsigned char uc;
+	for (uc = 1; uc > 0; uc++);
+	printf("Max char, unsigned = %u\n\n", --uc);
+
+	signed short s;
+	for (s = 1; s > 0; s++);
+	printf("Max short, signed = %d\n", --s);
+	for (s = -1; s < 0; s--);
+	printf("Min short, signed = %d\n", ++s);
+	unsigned short us;
+	for (us = 1; us > 0; us++);
+	printf("Max short, unsigned = %u\n\n", --us);
+
+	signed int i;
+	for (i = 1; i > 0; i++);
+	printf("Max int, signed = %d\n", --i);
+	for (i = -1; i < 0; i--);
+	printf("Min int, signed = %d\n", ++i);
+	unsigned int ui;
+	for (ui = 1; ui > 0; ui++);
+	printf("Max int, unsigned = %u\n\n", --ui);
+
+	signed long l;
+	for (l = 1; l > 0; l++);
+	printf("Max long, signed = %d\n", --l);
+	for (l = -1; l < 0; l--);
+	printf("Min long, signed = %d\n", ++l);
+	unsigned long ul;
+	for (ul = 1; ul > 0; ul++);
+	printf("Max long, unsigned = %u\n\n", --ul);
 
 }
