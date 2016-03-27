e8f6c014a83cb26b18da980c29591028a8aa6183
timwalsh300 <timwalsh300@gmail.com>
2016-03-26 21:30:45 -0500

    Finished 2-3

--- 2-3.c
+++ 2-3.c
@@ -8,18 +8,56 @@
 #define MAXLINE 100
 
 int htoi(char s[]);
+int input_length;
 
 int main() {
 
 	char line[MAXLINE];
 	printf("Enter a hexadecimal value: ");
-	//printf("Number of characters input: %d\n", mygetline(line, MAXLINE));
-	printf("\nThe integer value is %d\n", htoi(line));
+	input_length = mygetline(line, MAXLINE);
+	if (input_length > 0) {
+		printf("The integer value is %d\n", htoi(line));
+	}
 }
+
+int decode_digit(char c);
 
 int htoi(char s[]) {
 
 	int v = 0;
-	
+	int i = 0;
+	/* skip over leading '0x' or '0X' */
+	if (input_length > 2) {
+		if (s[i] == '0' && (s[i + 1] == 'x' || s[i + 1] == 'X')) {
+			i += 2;
+		}
+	}
+	/* convert actual hexadecimal digits */
+	for ( ; i < input_length; i++) {
+		v = 16 * v + decode_digit(s[i]);
+	}
 	return v;
+}
+
+int decode_digit(char c) {
+
+	switch(c) {
+		case '0': return 0;
+		case '1': return 1;
+		case '2': return 2;
+		case '3': return 3;
+		case '4': return 4;
+		case '5': return 5;
+		case '6': return 6;
+		case '7': return 7;
+		case '8': return 8;
+		case '9': return 9;
+		case 'a': case 'A': return 10;
+		case 'b': case 'B': return 11;
+		case 'c': case 'C': return 12;
+		case 'd': case 'D': return 13;
+		case 'e': case 'E': return 14;
+		case 'f': case 'F': return 15;
+		default:  return -1;
+	}
 }
