d0b97ec4deeb00152506b0978881b5d7c234cc0f
timwalsh300 <timwalsh300@gmail.com>
2016-03-27 21:47:25 -0500

    Finished 2-5

---
+++ 2-5.c
@@ -0,0 +1,32 @@
+/* Page 48: Write a function any(char s1[], char s2[]) that returns the first
+** location in s1 where any character from s2 occurs, or -1 if there is no match
+*/
+
+#include <stdio.h>
+#include "mygetline.h"
+#define MAXLINE 100
+
+int any(char s1[], char s2[]);
+
+int main() {
+
+	char s1[MAXLINE], s2[MAXLINE];
+	printf("Enter string #1: ");
+	mygetline(s1, MAXLINE);
+	printf("Enter string #2: ");
+	mygetline(s2, MAXLINE);
+	printf("The first position of any match is %d\n", any(s1, s2));
+}
+
+int any(char s1[], char s2[]) {
+
+	for (int i = 0; s2[i] != '\0'; i++) {
+		for (int j = 0; s1[j] != '\0'; j++) {
+			if (s1[j] == s2[i]) {
+				return j;
+			}
+		}
+	}
+	return -1;
+}
+
