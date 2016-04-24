044d2357dcb0c28f2068e203e77c42c6d65d1453
timwalsh300 <timwalsh300@gmail.com>
2016-04-24 14:01:57 -0500

    Finished 3-5; converts up to base 36

---
+++ 3-5.c
@@ -0,0 +1,58 @@
+/* Page 64: Write a function itob(n, s, b) which converts an integer n into a base-b
+** character representation in the string s. For example, itob(n, s, 16) formats n
+** as a hexadecimal string in s.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "mygetline.h"
+#define MAXLINE 10
+
+int itob(int n, char s[], int b);
+
+int main() {
+
+	char integer_input_string[MAXLINE];
+	char base_string[MAXLINE];
+	char output_string[MAXLINE];
+
+	printf("Enter a decimal integer to convert: ");
+	mygetline(integer_input_string, MAXLINE);
+	printf("Enter the new base: ");
+	mygetline(base_string, MAXLINE);
+	itob(atoi(integer_input_string), output_string, atoi(base_string));
+	printf("The new representation is: %s\n", output_string);
+}
+
+void reverse(char s[]);
+
+int itob(int n, char s[], int b) {
+
+	char charset[36] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	int i, sign;
+	if ((sign = n) < 0) {
+		n = -n;
+	}
+	i = 0;
+	do {
+		s[i++] = charset[n % b];
+	} while ((n /= b) > 0);
+	if (sign < 0) {
+		s[i++] = '-';
+	}
+	s[i] = '\0';
+	reverse(s);
+}
+
+void reverse(char s[]) {
+
+	int c, i, j;
+	for (i = 0, j = strlen(s) - 1; i < j; i++, j--) {
+		c = s[j];
+		s[j] = s[i];
+		s[i] = c;
+	}
+}
+
