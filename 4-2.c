0f0c192f3a889dfcb67b2c5ddff61db961f5b9be
timwalsh300 <timwalsh300@gmail.com>
2016-04-28 20:49:19 -0500

    Finished 4-2.c; doing conversion in reverse would be interesting

---
+++ 4-2.c
@@ -0,0 +1,69 @@
+/* Page 73: Extend atof on page 71 to handle scientific notation of
+** the form 123.45e-6 where a floating point number may be followed
+** by e or E and an optionally signed exponent
+*/
+
+#include <stdio.h>
+#include <ctype.h>
+#include "mygetline.h"
+#define MAXLINE 100
+
+double atof(char s[]);
+
+int main() {
+
+	char input_string[MAXLINE];
+	printf("Enter a number of the form XX.XX or X.Xe-XX: ");
+	mygetline(input_string, MAXLINE);
+	printf("The float version is %f\n", atof(input_string));
+}
+
+double atof(char s[]) {
+
+	double val, power;
+	int i, sig_sign, exp;
+	int negative_exp = 0;
+	for (i = 0; isspace(s[i]); i++)
+		;
+	sig_sign = (s[i] == '-') ? -1 : 1;
+	if (s[i] == '+' || s[i] == '-') {
+		i++;
+	}
+	for (val = 0.0; isdigit(s[i]); i++) {
+		val = 10.0 * val + (s[i] - '0');
+	}
+	if (s[i] == '.') {
+		i++;
+		for (power = 1.0; isdigit(s[i]); i++) {
+			val = 10.0 * val + (s[i] - '0');
+			power *= 10.0;
+		}
+	}
+	val = sig_sign * val / power;
+
+	if (s[i] == 'e' || s[i] == 'E') {
+		i++;
+		if (s[i] == '-') {
+			negative_exp = 1;
+			i++;
+		}
+		//parse the digits after e/E
+		for (exp = 0; isdigit(s[i]); i++) {
+			exp = 10 * exp + (s[i] - '0');
+		}
+		//10 ^ e/E
+		power = 1.0;
+		for (int k = 0; k < exp; k++) {
+			power *= 10.0;
+		}
+		//multiply or divide significand by (10 ^ e/E)
+		if (negative_exp) {
+			val /= power;
+		}
+		else {
+			val *= power;
+		}
+	}
+
+	return val;
+}
