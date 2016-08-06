9db065dcfeba879244477cf0bdf95895179e20a8
timwalsh300 <timwalsh300@gmail.com>
2016-08-06 15:23:28 -0500

    5-8 works, but must check leap year errors sooner

--- 5-8.c
+++ 5-8.c
@@ -39,45 +39,76 @@ int day_of_year(int, int, int);
 
 int day_of_year_helper(void)
 {
-		char input[MAXLINE], year_str[5], month_str[3], day_str[3];
-		int year_int, month_int, day_int;
+	char input[MAXLINE], year_str[5], month_str[3], day_str[3];
+	int year_int, month_int, day_int;
 
-		printf("Enter the date as yyyymmdd: ");
-		mygetline(input, MAXLINE);
-		if (strlen(input) != 8) {
-			printf("%s is not a valid date format\n", input);
-		}
+	printf("Enter the date as yyyymmdd: ");
+	mygetline(input, MAXLINE);
+	if (strlen(input) != 8) {
+		printf("%s is not a valid date format\n", input);
+	}
 
-		strncpy(year_str, input, 4);
-		year_str[4] = '\0';
-		year_int = atoi(year_str);
+	strncpy(year_str, input, 4);
+	year_str[4] = '\0';
+	year_int = atoi(year_str);
 
-		strncpy(month_str, input + 4, 2);
-		month_str[2] = '\0';
-		month_int = atoi(month_str);
+	strncpy(month_str, input + 4, 2);
+	month_str[2] = '\0';
+	month_int = atoi(month_str);
 
-		strncpy(day_str, input + 6, 2);
-		day_str[2] = '\0';
-		day_int = atoi(day_str);
+	strncpy(day_str, input + 6, 2);
+	day_str[2] = '\0';
+	day_int = atoi(day_str);
 
-		if (month_int < 1 || month_int > 12) {
-			printf("%s is not a valid input for the month\n", month_str);
-			return -1;
-		}
-		if (day_int < 1 || day_int > daytab[1][month_int]) {
-			printf("%s is not a valid input for the day\n", day_str);
-			return -1;
-		}
+	if (month_int < 1 || month_int > 12) {
+		printf("%s is not a valid input for the month\n", month_str);
+		return -1;
+	}
+	if (day_int < 1 || day_int > daytab[1][month_int]) {
+		printf("%s is not a valid input for the day\n", day_str);
+		return -1;
+	}
 
-		return day_of_year(year_int, month_int, day_int);
+	return day_of_year(year_int, month_int, day_int);
 }
 
 void month_day(int, int, int *, int *);
 
 void month_day_helper(char *output)
 {
-	
-	
+	char num_str[MAXLINE], year_str[MAXLINE], month_str[MAXLINE], day_str[MAXLINE];
+	int *day = malloc(sizeof(int));
+	int *month = malloc(sizeof(int));
+
+	printf("Enter the number day of the year: ");
+	mygetline(num_str, MAXLINE);
+	printf("Enter the year: ");
+	mygetline(year_str, MAXLINE);
+
+	if (atoi(num_str) < 1 || atoi(num_str) > 366) {
+		printf("%s is not a valid numbered day\n", num_str);
+		sprintf(output, "ERROR");
+		free(day);
+		free(month);
+		return;
+	}
+
+	month_day(atoi(year_str), atoi(num_str), month, day);
+
+	if (*month < 10) {
+		sprintf(month_str, "0%d", *month);
+	} else {
+		sprintf(month_str, "%d", *month);
+	}
+	if (*day < 10) {
+		sprintf(day_str, "0%d", *day);
+	} else {
+		sprintf(day_str, "%d", *day);
+	}
+	sprintf(output, "%s%s%s", year_str, month_str, day_str);
+
+	free(day);
+	free(month);
 }
 
 int day_of_year(int year, int month, int day)
